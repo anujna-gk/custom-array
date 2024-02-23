9fe51e1d0daf19870bed89a927072ee61fba71de
anujnaaa <anujnak282@gmail.com>
2024-02-23 15:30:13 +0530

    custom array created

---
+++ index.js
@@ -0,0 +1,60 @@
+function Array() {
+  this.sizeproperty = arguments.length;
+
+  // console.log(this.sizeproperty);
+  // console.log(this);
+  // console.log(typeof arguments);
+  // console.log(arguments);
+  // console.log(this.arguments);
+  // console.log(Array.arguments);
+  // console.log(this.Array);
+  // console.log(Array);
+  console.log("//////////////////////////");
+  for (let i = 0; i < arguments.length; i++) {
+    this[i] = arguments[i];
+  }
+}
+
+Array.prototype.custompush = function (item) {
+  //console.log("hey");
+  // console.log(this.sizeproperty);
+  last = this.sizeproperty;
+  this[last] = item;
+  this.sizeproperty++;
+  console.log(this);
+};
+
+Array.prototype.custompop = function () {
+  last = this.sizeproperty;
+  //console.log(last);
+  if (last === 0) {
+    console.log("empaty array");
+    return;
+  } else {
+    delete this[last - 1];
+    //console.log("heyyy2");
+    this.sizeproperty--;
+    console.log(this);
+  }
+};
+
+console.log("obj 1");
+const array1 = new Array(5, 6, 7, 8);
+console.log(array1);
+//console.log(array1);
+array1.custompush(9);
+//console.log(array1);
+array1.custompop();
+//console.log(array1);
+
+console.log("obj 2");
+const array2 = new Array();
+//console.log(array2);
+array2.custompop();
+
+console.log("obj 3");
+const array3 = new Array(7, 8, 4, 5, 2, 3, 78, 54, 23, 45);
+console.log(array3);
+array3.custompop();
+array3.custompop();
+array3.custompush(28, 29);
